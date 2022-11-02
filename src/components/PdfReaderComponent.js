d9801bb0f7ba628ac714bc9c24171255e43a1c8d
gabrielekarra <gabrielekarra@hotmail.it>
2022-11-02 21:23:08 +0100

    PL-34-Knowledge graph can be shown in a separate window
    Added floating button for download/upload(change pdf)/popup the graph

--- src/components/PdfReaderComponent.js
+++ src/components/PdfReaderComponent.js
@@ -1,123 +1,124 @@
-import { EventHandlerService, PDFLEvents } from '../services/EventHandlerService';
-import { PaginationComponent } from './PaginationComponent';
-import { ZoomComponent } from './ZoomComponent';
+import {
+  EventHandlerService,
+  PDFLEvents,
+} from "../services/EventHandlerService";
+import { PaginationComponent } from "./PaginationComponent";
+import { ZoomComponent } from "./ZoomComponent";
+import { SideBarComponent } from "./SideBarComponent";
 
 const pdfjsLib = require("pdfjs-dist");
 
 class PdfReaderComponent {
+  components = {
+    pdfContainer: document.querySelector("#pdf_container"),
+    openNew: document.querySelector("#open_new"),
+  };
 
-    components = {
-        pdfContainer: document.querySelector('#pdf_container'),
-        openNew: document.querySelector('#open_new')
-    }
+  /**
+   * @constructor
+   */
+  constructor() {
+    this.paginationComponent = new PaginationComponent();
+    this.zoomComponent = new ZoomComponent();
 
-    /**
-     * @constructor
-     */
-    constructor() {
-        this.paginationComponent = new PaginationComponent();
-        this.zoomComponent = new ZoomComponent();
+    this.#registerEvents();
+  }
 
-        this.#registerEvents();
-    }
+  /**
+   * Add event listener to view elements of the toolbar
+   */
+  #registerEvents = () => {
+    this.components.openNew.addEventListener("click", this.#onNewFile);
 
-    /**
-     * Add event listener to view elements of the toolbar
-     */
-    #registerEvents = () => {
-        this.components.openNew.addEventListener('click', this.#onNewFile);
+    EventHandlerService.subscribe(PDFLEvents.onRenderPage, () => {
+      this.#renderPage();
+    });
+  };
 
-        EventHandlerService.subscribe(PDFLEvents.onRenderPage, () => {
-            this.#renderPage();
+  #onNewFile = () => {
+    EventHandlerService.publish(PDFLEvents.onShowInputView);
+  };
+
+  /**
+   * Load and render the first page of the given pdf
+   * @param pdf data, filename or url of a PDF document
+   */
+  loadPdf = (pdf) => {
+    const self = this;
+    pdfjsLib.GlobalWorkerOptions.workerSrc = "webpack/pdf.worker.bundle.js";
+    pdfjsLib
+      .getDocument(pdf)
+      .promise.then((data) => {
+        self.pdfDoc = data;
+        self.paginationComponent.setPageCount(data.numPages);
+        self.#renderPage();
+      })
+      .catch((err) => {
+        console.log(err.message); // TODO: handle error in some way
+      });
+  };
+
+  /**
+   * Private function, render the page
+   */
+  #renderPage = () => {
+    const self = this;
+    this.pdfDoc
+      .getPage(self.paginationComponent.getCurrentPage())
+      .then((page) => {
+        //Set the HTML properties
+        const canvas = document.createElement("canvas");
+
+        canvas.setAttribute("class", "canvas__container");
+        const textLayer = document.createElement("div");
+        textLayer.setAttribute("class", "textLayer");
+
+        const ctx = canvas.getContext("2d");
+        const viewport = page.getViewport({
+          scale: self.zoomComponent.getZoom(),
         });
-    }
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
 
-    #onNewFile = () => {
-        EventHandlerService.publish(PDFLEvents.onShowInputView);
-    }
+        // Render the PDF page into the canvas context.
+        const renderCtx = {
+          canvasContext: ctx,
+          viewport: viewport,
+        };
 
-    /**
-     * Load and render the first page of the given pdf
-     * @param pdf data, filename or url of a PDF document
-     */
-    loadPdf = (pdf) => {
-        const self = this;
-        pdfjsLib.GlobalWorkerOptions.workerSrc = "webpack/pdf.worker.bundle.js";
-        pdfjsLib.getDocument(pdf).promise.then((data) => {
-            self.pdfDoc = data;
-            self.paginationComponent.setPageCount(data.numPages);
-            self.#renderPage();
-        }).catch((err) => {
-            console.log(err.message); // TODO: handle error in some way
-        });
-    }
+        var renderTask = page.render(renderCtx);
 
-    /**
-     * Private function, render the page 
-     */
-    #renderPage = () => {
-        const self = this;
-        this.pdfDoc
-            .getPage(self.paginationComponent.getCurrentPage())
-            .then((page) => {
-                //Set the HTML properties
-                const canvas = document.createElement("canvas");
+        renderTask.promise.then(function () {
+          page.getTextContent().then(function (textContent) {
+            textLayer.style.left = canvas.offsetLeft + "px";
+            textLayer.style.top = canvas.offsetTop + "px";
+            textLayer.style.height = canvas.offsetHeight + "px";
+            textLayer.style.width = canvas.offsetWidth + "px";
 
-                canvas.setAttribute('class', 'canvas__container');
-                const textLayer = document.createElement("div");
-                textLayer.setAttribute('class', 'textLayer')
-
-                const ctx = canvas.getContext('2d');
-                const viewport = page.getViewport({
-                    scale: self.zoomComponent.getZoom(),
-                });
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                // Render the PDF page into the canvas context.
-                const renderCtx = {
-                    canvasContext: ctx,
-                    viewport: viewport,
-                };
-
-                var renderTask = page.render(renderCtx);
-
-                renderTask.promise.then(function () {
-
-                    page.getTextContent().then(function (textContent) {
-
-                        textLayer.style.left = canvas.offsetLeft + 'px';
-                        textLayer.style.top = canvas.offsetTop + 'px';
-                        textLayer.style.height = canvas.offsetHeight + 'px';
-                        textLayer.style.width = canvas.offsetWidth + 'px';
-
-                        pdfjsLib.renderTextLayer({
-                            textContent: textContent,
-                            container: textLayer,
-                            viewport: viewport,
-                            textDivs: []
-                        });
-
-                    });
-                })
-
-
-                page.render(renderCtx);
-
-                if (ctx) {
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.beginPath();
-                }
-
-                //Scroll is possible but not supported by other navigation functions, clear container before adding the new page
-                self.components.pdfContainer.innerHTML = "";
-                self.components.pdfContainer.appendChild(canvas);
-                self.components.pdfContainer.appendChild(textLayer);
-
-                self.paginationComponent.setCurrentPage();
+            pdfjsLib.renderTextLayer({
+              textContent: textContent,
+              container: textLayer,
+              viewport: viewport,
+              textDivs: [],
             });
-    }
+          });
+        });
 
+        page.render(renderCtx);
+
+        if (ctx) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.beginPath();
+        }
+
+        //Scroll is possible but not supported by other navigation functions, clear container before adding the new page
+        self.components.pdfContainer.innerHTML = "";
+        self.components.pdfContainer.appendChild(canvas);
+        self.components.pdfContainer.appendChild(textLayer);
+
+        self.paginationComponent.setCurrentPage();
+      });
+  };
 }
 
 export { PdfReaderComponent };
---
+++ src/components/SideBarComponent.js
@@ -0,0 +1,18 @@
+import {
+  EventHandlerService,
+  PDFLEvents,
+} from "../services/EventHandlerService";
+
+class SideBarComponent {
+  components = {};
+
+  constructor() {}
+
+  #registerEvents = () => {};
+
+  /**
+   * Callback for zoom in event
+   */
+}
+
+export { SideBarComponent };
