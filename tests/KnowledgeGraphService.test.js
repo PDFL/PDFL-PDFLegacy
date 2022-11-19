32066d9f469ac30fff5f4207ca49ae665c8d2c64
Tonio Ercegović <tonio.ercegovic@fer.hr>
2022-11-19 01:21:13 +0100

    rebase test

--- tests/KnowledgeGraphService.test.js
+++ tests/KnowledgeGraphService.test.js
@@ -3,7 +3,6 @@ import { fetchPaperInfo } from "../src/services/Api";
 import {
   getCitations,
   getReferences,
-  getPaperInfo,
 } from "../src/services/KnowledgeGraphService";
 
 const TITLE =
@@ -12,7 +11,7 @@ const PAPER_ID = "e21c794b4941a5628b3b8c138e211a5b75b66a08";
 // This paper has this paper_id, 1 citation and 74 references
 
 test("Tests KnowledgeGraphService.getPaperID", async () => {
-  let paperInfo = await getPaperInfo(TITLE);
+  let paperInfo = await fetchPaperInfo(TITLE);
 
   expect(paperInfo.title).toBe(TITLE);
   expect(paperInfo.paperId).toBe(PAPER_ID);
@@ -20,7 +19,7 @@ test("Tests KnowledgeGraphService.getPaperID", async () => {
 
 test("Tests KnowledgeGraphService.getPaperID, title does not exist", async () => {
   let title = "Meow meow meow meow moew";
-  let paperInfo = await getPaperInfo(title);
+  let paperInfo = await fetchPaperInfo(title);
 
   expect(paperInfo).not.toBe(title);
   expect(paperInfo.paperId.length).toBe(PAPER_ID.length);
