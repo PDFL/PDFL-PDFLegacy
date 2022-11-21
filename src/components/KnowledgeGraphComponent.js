312f09b0a0a238598f6d5d39af83284cba3e4ec2
gabrielekarra <gabrielekarra@hotmail.it>
2022-11-21 16:10:41 +0100

    PL-88-Knowledge-design-improvements
    - graph new design
    - knowledge graph failure display
    - loading display

--- src/components/KnowledgeGraphComponent.js
+++ src/components/KnowledgeGraphComponent.js
@@ -1,3 +1,4 @@
+import { GroupAddSharp, OfflinePinRounded } from "@mui/icons-material";
 import ForceGraph from "force-graph";
 import { MAX_GRAPH_DEPTH } from "../Constants";
 import { nodesMock, linksMock } from "../mocks/KnowledgeGaphMocks";
@@ -39,13 +40,12 @@ class KnowledgeGraphComponent {
         .nodeAutoColorBy("group")
         .nodeCanvasObject((node, ctx, globalScale) => {
           //TODO: add custom style
-
           const label = node.label.substring(0, 4);
-          const fontSize = 10;
+          const fontSize = 14 / globalScale;
           ctx.font = `${fontSize}px Sans-Serif`;
           const textWidth = ctx.measureText(label).width;
           const bckgDimensions = [textWidth, fontSize].map(
-            (n) => n + fontSize * 0.4
+            (n) => n + fontSize * 0.6
           ); // some padding
 
           ctx.fillStyle = "#489c8a";
@@ -73,22 +73,24 @@ class KnowledgeGraphComponent {
               ...bckgDimensions
             );
         })
-        .linkCurvature("curvature")
-        .linkDirectionalArrowLength(10)
-        .linkDirectionalArrowRelPos(2)
-        .linkDirectionalParticles(2)
+        .linkCurvature(0.06)
+        .linkDirectionalArrowLength(7)
+        .linkDirectionalArrowRelPos(0.5)
+        .linkDirectionalParticles(1)
         .linkDirectionalParticleWidth(5)
-        .cooldownTime([300])
+        .linkDirectionalParticleColor(["#2980b9"])
+        .linkDirectionalArrowColor(["#2980b9"])
+        .cooldownTime(300)
         .d3Force("center", null)
         .onEngineStop(() => graph.zoomToFit(300));
+
+      buildGraphProcedure(graph, MAX_GRAPH_DEPTH);
 
       graph.onNodeClick((node) => {
         // Center/zoom on node
         graph.centerAt(node.x, node.y, 1000);
         graph.zoom(4, 2000);
       });
-
-      buildGraphProcedure(graph, MAX_GRAPH_DEPTH);
     });
   };
 }
