87ba8e4e33a542efce73d5d94b2a6c856d6166d5
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-11-22 11:38:17 +0100

    Add documentation

--- src/components/ReferenceComponent.js
+++ src/components/ReferenceComponent.js
@@ -5,17 +5,33 @@ import { TextExtractorService } from "../services/DocumentParser/TextExtractorSe
 import {ExternalCitationExtractorService} from "../services/DocumentParser/ExternalCitationExtractorService";
 import {TableExtractorService} from "../services/DocumentParser/TableExtractorService";
 
+/**
+ * This class handles user interaction with internal document references
+ * @property {object} pdfDocument
+ */
 class ReferenceComponent {
 
+  /**
+   * @constructor
+   * Subscribes to an event to be notified when links are available in dom
+   */
   constructor() {
     this.pdfDoc = null;
     EventHandlerService.subscribe(PDFLEvents.onLinkLayerRendered, this.#onLinkLayerRendered.bind(this));
   }
 
+  /**
+   * Set the pdf document
+   * @param pdfDoc
+   */
   setPdfDoc = (pdfDoc) => {
     this.pdfDoc = pdfDoc;
   }
 
+  /**
+   * @private
+   * Get the a tags from the DOM and add the event listener both for click and onMouseOver with delay
+   */
   #onLinkLayerRendered = () => {
     if(this.pdfDoc === null){ throw new Error('PDFDocument object missed'); }
     const pageHref = document.getElementsByClassName('internalLink');
@@ -26,6 +42,11 @@ class ReferenceComponent {
     }
   }
 
+  /**
+   * Function for handling the onClick event of a reference
+   * Solve the reference and publish and event to require the page change
+   * @param event
+   */
   #onInternalReferenceClick = (event) => {
     const self = this;
     event.preventDefault();
@@ -37,6 +58,10 @@ class ReferenceComponent {
     });
   };
 
+  /**
+   * Function for handling the onMouseOver event of a reference
+   * @param event
+   */
   #onInternalReferenceOver = (event) => {
     const self = this;
     event.preventDefault();
@@ -48,6 +73,11 @@ class ReferenceComponent {
     });
   };
 
+  /**
+   * Given ad id the function return a promise with the correct page number of the reference
+   * @param refId the reference ID from the dom
+   * @returns {Promise<number>}
+   */
   #solveReference = async (refId) => {
     const self = this;
     const destinationObject = await self.pdfDoc.getDestination(refId);
@@ -55,6 +85,11 @@ class ReferenceComponent {
     return pageIndex + 1;
   }
 
+  /**
+   * Call the correct parser and rise ad event to notify that popup content is ready
+   * @param reference the reference from the dom
+   * @param pageNumber the solved page number
+   */
   #parseReference = (reference, pageNumber) => {
     const self = this;
     const referenceType = reference.split('.')[0];
