3ffbb5c4b92f53919ab71a46832aa56c394f8135
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-12-01 18:01:41 +0100

    Full screen (alt + L) and KnowledgeGraph (ctrl + G)

--- src/DisabledDefaultKeyboardShortcuts.js
+++ src/DisabledDefaultKeyboardShortcuts.js
@@ -1,7 +1,8 @@
 export const PreventOnKeyUp = [];
 export const PreventOnKeyDown = [
-    {ctrl: true, alt: false, shift: false, key: 187},
-    {ctrl: true, alt: false, shift: false, key: 189},
-    {ctrl: true, alt: false, shift: false, key: 85},
+  { ctrl: true, alt: false, shift: false, key: 187 },
+  { ctrl: true, alt: false, shift: false, key: 189 },
+  { ctrl: true, alt: false, shift: false, key: 85 },
+  { ctrl: true, alt: false, shift: false, key: 71 },
 ];
 export const PreventOnKeyPress = [];
