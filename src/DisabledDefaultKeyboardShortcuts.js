154a08528efe018dd48e9e29af224aae04824c5d
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-12-01 17:49:57 +0100

    Fixes

---
+++ src/DisabledDefaultKeyboardShortcuts.js
@@ -0,0 +1,7 @@
+export const PreventOnKeyUp = [];
+export const PreventOnKeyDown = [
+    {ctrl: true, alt: false, shift: false, key: 187},
+    {ctrl: true, alt: false, shift: false, key: 189},
+    {ctrl: true, alt: false, shift: false, key: 85},
+];
+export const PreventOnKeyPress = [];
