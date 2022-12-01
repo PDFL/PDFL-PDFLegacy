c66c3a4d34e7f96045315d202a5bae4634e49d4c
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-12-01 18:35:01 +0100

    More documentation

--- src/DisabledDefaultKeyboardShortcuts.js
+++ src/DisabledDefaultKeyboardShortcuts.js
@@ -1,11 +1,11 @@
 /**
  * Keyboard key combination to be prevented by default action for keyUp event
- * @type {[{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number}]}
+ * @type {[{ctrl: boolean, shift: boolean, alt: boolean, key: number}]}
  */
 export const PreventOnKeyUp = [];
 /**
  * Keyboard key combination to be prevented by default action for keyDown event
- * @type {[{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number}]}
+ * @type {[{ctrl: boolean, shift: boolean, alt: boolean, key: number}]}
  */
 export const PreventOnKeyDown = [
   { ctrl: true, alt: false, shift: false, key: 187 },
@@ -16,6 +16,6 @@ export const PreventOnKeyDown = [
 /**
  * Keyboard key combination to be prevented by default action for keyPress event
  * Only ctrl or cmd or alt or shift are disabled by default.
- * @type {[{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number},{ctrl: boolean, shift: boolean, alt: boolean, key: number}]}
+ * @type {[{ctrl: boolean, shift: boolean, alt: boolean, key: number}]}
  */
 export const PreventOnKeyPress = [];
