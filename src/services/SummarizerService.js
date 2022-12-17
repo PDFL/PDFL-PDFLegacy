6d91c858f38b0142ae5fe14ad4b125af2d15d111
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-12-17 17:06:10 +0100

    Add other version of the same service

--- src/services/SummarizerService.js
+++ src/services/SummarizerService.js
@@ -44,3 +44,47 @@ class TextSummarizer {
 }
 
 export { TextSummarizer };
+
+/*Other version below*/
+
+const SummaryTool = require("node-summary");
+
+/**
+ * Service to create a summary of a given text and of a given number of sentences
+ * @param {SummarizerManager} summarizer instance of summarizer manager object from node-summarizer library
+ */
+class TextSummary {
+  /**
+   * @constructor
+   * Initialize the service with the text and the desired length
+   * @param title {string} the title of content to be summarized
+   * @param text {string} text to be summarized
+   */
+  constructor(title, text) {
+    this.title = title;
+    this.text = text;
+  }
+
+  /**
+   * Summarize the text given in the constructor
+   * @returns {Promise<{summary: string, summaryRation: float}>}
+   */
+  getSummary = async () => {
+    const self = this;
+    return new Promise((resolve) => {
+      SummaryTool.summarize(self.title, self.text, function (error, summary) {
+        if (error) {
+          throw new Error("Unable to create summary");
+        }
+        resolve({
+          summary: summary,
+          summaryRatio:
+            100 -
+            100 * (summary.length / (self.title.length + self.text.length)),
+        });
+      });
+    });
+  };
+}
+
+export { TextSummary };
