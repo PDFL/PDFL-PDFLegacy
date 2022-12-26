bf7adeb3fd75e3ab767a28b28070b61956d9f152
ninagnjidic <nina.gnjidic@fer.hr>
2022-12-26 20:07:50 +0100

    ThumbnailComponent documentation

--- src/components/ThumbnailComponent.js
+++ src/components/ThumbnailComponent.js
@@ -3,19 +3,35 @@ import {
   PDFLEvents,
 } from "../services/EventHandlerService";
 
-const pdfjsLib = require("pdfjs-dist");
-
+/**
+ * Component responsible for creating and displaying PDF thumbnail.
+ * The thumbnail contains all PDF pages which can be scrolled through and
+ * clicked.
+ * 
+ * @property {Object} components object that holds all DOM elements within this component
+ * @property {HTMLElement} components.toolbar element that represents this component
+ * @property {Object} pdfjsLib PDF.js library
+ * @property {boolean} isOpened state of component which can be either closed or opened
+ */
 class ThumbnailComponent {
   components = {
     thumbnail: document.querySelector("#thumbnail"),
   };
 
+  /**
+   * Creates and initializes new thumbnail component. Sets component state to closed.
+   */
   constructor() {
+    this.pdfjsLib = require("pdfjs-dist");
     this.isOpened = false;
 
     this.#registerEvents();
   }
 
+  /**
+   * Registers event listeners to this component.
+   * @private
+   */
   #registerEvents = () => {
     EventHandlerService.subscribe(PDFLEvents.onReadNewFile, (pdfDoc) =>
       this.#createNewThumbnail(pdfDoc)
@@ -26,38 +42,59 @@ class ThumbnailComponent {
     );
 
     EventHandlerService.subscribe(PDFLEvents.onPageChanged, (pageNumber) =>
-      this.#setSelectedPage(
-        document.querySelectorAll(".thumbnail-page-canvas")[pageNumber - 1]
-      )
+      this.#setSelectedPage(document.querySelectorAll(".thumbnail-page-canvas")[pageNumber - 1])
     );
   };
 
+  /**
+   * Closes the thumbnail if it was opened and vice versa. Changes
+   * state accordingly.
+   * @private
+   */
   #toggle = () => {
     this.isOpened ? this.#close() : this.#open();
 
     this.isOpened = !this.isOpened;
   };
 
+  /**
+   * Displays thumbnail.
+   */
   #open = () => {
     this.components.thumbnail.classList.remove("hidden");
   };
 
+  /**
+   * Hides thumbnail.
+   */
   #close = () => {
     this.components.thumbnail.classList.add("hidden");
   };
 
+  /**
+   * Removes old and creates new thumbnail from PDF document data.
+   * @param {Uint8Array} pdfDoc PDF document data
+   */
   #createNewThumbnail = async (pdfDoc) => {
     this.#clear();
 
-    let document = await pdfjsLib.getDocument(pdfDoc).promise;
+    let document = await this.pdfjsLib.getDocument(pdfDoc).promise;
     for (let pageNumber = 1; pageNumber <= document.numPages; pageNumber++)
       await this.#createPage(await document.getPage(pageNumber), pageNumber);
   };
 
+  /**
+   * Removes all page elements from thumbnail element.
+   */
   #clear = () => {
     this.components.thumbnail.innerHTML = "";
   };
 
+  /**
+   * Creates new small PDF page in thumbnail. 
+   * @param {PDFPageProxy} page PDF page
+   * @param {int} num page number
+   */
   #createPage = async (page, num) => {
     let viewport = this.#createPageViewport(page);
     let canvas = this.#createPageCanvas(viewport, num);
@@ -67,12 +104,24 @@ class ThumbnailComponent {
       .promise.then(() => this.#createPageContainer(canvas, num));
   };
 
+  /**
+   * Returns page viewport scaled to 0.2.
+   * @param {PDFPageProxy} page PDF page
+   * @returns {Object} viewport
+   */
   #createPageViewport = (page) => {
     return page.getViewport({
       scale: 0.2,
     });
   };
 
+  /**
+   * Creates styled page canvas with click event.
+   * 
+   * @param {Object} viewport page viewport
+   * @param {int} pageNumber page number
+   * @returns {HTMLElement} canvas
+   */
   #createPageCanvas = (viewport, pageNumber) => {
     let canvas = document.createElement("canvas");
 
@@ -87,45 +136,87 @@ class ThumbnailComponent {
     return canvas;
   };
 
+  /**
+   * Adds canvas style depending if page is
+   * selected or not.
+   * 
+   * @param {HTMLElement} canvas page canvas
+   * @param {boolean} isSelected is page selected
+   */
   #setCanvasStyle(canvas, isSelected) {
     if (isSelected) this.#setSelectedCanvasStyle(canvas);
     else this.#setDefaultCanvasStyle(canvas);
   }
 
+  /**
+   * Sets style of selected page canvas. Selected page
+   * canvas has a blue border and 100% opacity.
+   * @param {HTMLElement} canvas page canvas
+   */
   #setSelectedCanvasStyle(canvas) {
     canvas.classList.remove("half-opacity");
     canvas.classList.add("full-opacity", "blue-border");
   }
 
+  /**
+   * Sets default style of page canvas. Default style
+   * of page has 50% opacity.
+   * @param {HTMLElement} canvas page canvas
+   */
   #setDefaultCanvasStyle(canvas) {
     canvas.classList.remove("full-opacity", "blue-border");
     canvas.classList.add("half-opacity");
   }
 
+  /**
+   * Callback for when page canvas  in thumbnail is clicked. Publishes
+   * an event to change page in reader and sets selected page.
+   * @param {HTMLElement} canvas page canvas
+   * @param {int} pageNumber page number
+   */
   #pageClicked = (canvas, pageNumber) => {
-    console.log("page clicked");
     EventHandlerService.publish(PDFLEvents.onNewPageRequest, pageNumber);
 
     this.#setSelectedPage(canvas);
   };
 
+  /**
+   * Sets selected page in thumbnail.
+   * @param {HTMLElement} canvas page canvas
+   */
   #setSelectedPage = (canvas) => {
     this.#unselectAllCanvases();
     this.#setSelectedCanvasStyle(canvas);
     this.#setScrollPosition(canvas);
   };
 
+  /**
+   * Sets default style to all page canvases in thumbnail.
+   */
   #unselectAllCanvases() {
     document.querySelectorAll(".thumbnail-page-canvas").forEach((canvas) => {
       this.#setDefaultCanvasStyle(canvas);
     });
   }
 
+  /**
+   * Sets scroll position to display given canvas. If canvas is already
+   * visible scroll does not change and if it is not visible scroll position
+   * is changed so it is visible in the bottom of thumbnail.
+   * @param {HTMLElement} canvas page canvas
+   */
   #setScrollPosition = (canvas) => {
     canvas.parentElement.scrollIntoView({ block: "nearest" });
     canvas.parentElement.focus({ preventScroll: true });
   };
 
+  /**
+   * Creates page container which contains page canvas
+   * and page number below it.
+   * @param {HTMLElement} canvas page canvas
+   * @param {int} num page number
+   * @returns {HTMLElement} page container
+   */
   #createPageContainer = (canvas, num) => {
     let container = document.createElement("div");
 
@@ -138,6 +229,11 @@ class ThumbnailComponent {
     return container;
   };
 
+  /**
+   * Creates container which only contains page number.
+   * @param {int} num page number
+   * @returns {HTMLElement} page number container
+   */
   #createPageNumberContainer = (num) => {
     let numberDiv = document.createElement("div");
 
