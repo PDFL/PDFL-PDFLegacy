3a7a5c75ddd64928fad96a7ff6545f90f5d82d5d
Tonio Ercegović <tonio.ercegovic@fer.hr>
2022-12-27 23:27:44 +0100

    PL-163: Changing pdfs is not resetting properly
    DONE:
    Fixed bug when pages not rendering properly when changing pdfs
    Correct page when switching pdf
    Knowledge graph and summary when switching pdf reset
    TODO:
    Close reference view when switching pdfs

--- src/components/PdfReaderComponent.js
+++ src/components/PdfReaderComponent.js
@@ -99,28 +99,25 @@ class PdfReaderComponent {
   };
 
   /**
-   * Load and render the first page of the given pdf.
+   * Loads the pdf document, configures 'child' components, delegates
+   * page setup and rendering of the first page.
    * @param {Uint8Array} pdf data, filename or url of a PDF document
    */
-  loadPdf = (pdf) => {
-    const self = this;
-    pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.bundle.js";
-    pdfjsLib
-      .getDocument(pdf)
-      .promise.then((data) => {
-        self.pdfDoc = data;
-        self.referenceComponent.setPdfDoc(data);
-        self.toolbarComponent.setPageCount(data.numPages);
-        self.sidePageComponent.setPDF(data);
-
-        self.referenceViewComponent.setPdfDoc(data);
-
-        this.#setupPages();
-      })
-      .catch((err) => {
-        console.log(err.message); // TODO: handle error in some way
-      });
+  loadPdf = async (pdf) => {
     this.components.loader.classList.add("hidden");
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.bundle.js";
+
+    let data = await pdfjsLib.getDocument(pdf).promise;
+
+    this.pdfDoc = data;
+    this.referenceComponent.setPdfDoc(data);
+    this.toolbarComponent.setPageCount(data.numPages);
+    this.sidePageComponent.setPDF(data);
+    this.referenceViewComponent.setPdfDoc(data);
+
+    await this.#setupPages();
+    this.#renderPages(1);
   };
 
   /**
@@ -129,9 +126,12 @@ class PdfReaderComponent {
    */
   reset = () => {
     this.sidePageComponent.hideSidePage();
+    this.sidePageComponent.hideSidePageSummary();
     this.toolbarComponent.reset();
     this.pages = [];
+    this.visiblePages = [];
     this.visiblePage = null;
+    window.scrollTo(0, 0);
   };
 
   /**
