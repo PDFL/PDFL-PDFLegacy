1591b33c0ed9345d75db91ef5ac0075648ac42aa
Tonio Ercegovic <tonio.ercegovic@fer.hr>
2022-12-28 20:23:51 +0100

    added close button and a panel for the two injected buttons

--- browser-extension/pdfPopup.js
+++ browser-extension/pdfPopup.js
@@ -1,20 +1,37 @@
+/**
+ * Generates the injected HTML showing two buttons,
+ * one for opening the pdf in PDFL reader and
+ * one for closing this popup.
+ */
 function generateOpenWithPDFLButton() {
+  var popup = document.createElement("div");
+  popup.setAttribute("id", "injected-popup");
+
   var btn = document.createElement("button");
+  btn.setAttribute("id", "injected-button");
   var t = document.createTextNode("Open in PDF - Legacy");
   btn.appendChild(t);
-  btn.style.zIndex = 100;
-  btn.style.top = "15px";
-  btn.style.left = "15px";
-  btn.style.position = "absolute";
+
+  var noBtn = document.createElement("button");
+  noBtn.setAttribute("id", "injected-button-no");
+  t = document.createTextNode("Close");
+  noBtn.appendChild(t);
 
   btn.addEventListener("click", () => {
     let url = location.href;
-    location.href = `localhost:8080/?url=${url}`;
+    location.href = `http://localhost:8080/?url=${url}`;
   });
-  document.body.appendChild(btn);
+  noBtn.addEventListener("click", () => {
+    popup.remove();
+  });
+
+  popup.appendChild(btn);
+  popup.append(noBtn);
+  document.body.appendChild(popup);
 }
 
 let url = location.href;
+// if pdf is open inject the DOM with popup window
 if (url.includes(".pdf") && !url.includes("pdfl")) {
   generateOpenWithPDFLButton();
 }
