e34df2b52bd0e735b11e89c0c415e1d6b5bc28ce
dmesic <dario.mesic@fer.hr>
2022-12-29 18:26:49 +0100

    improved design for navbar and added open pdf "button"

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -31,7 +31,7 @@ class NavbarComponent {
    * @private
    */
   #registerEvents = () => {
-    //this.components.openNew.addEventListener("input", this.#onNewFile);
+    this.components.openNew.addEventListener("input", this.#onNewFile);
   };
 
   /**
