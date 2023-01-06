de2987f1094d885f35d010a9f022e75a9449d5db
Tonio Ercegovic <tonio.ercegovic@fer.hr>
2023-01-06 16:50:33 +0100

    knowledge graph e2e tests

---
+++ cypress/e2e/knowledgeGraph.cy.js
@@ -0,0 +1,31 @@
+describe("Knowledge Graph functionallity (TA-14 to TA-16", () => {
+  beforeEach(() => {
+    cy.visit("https://pdfl-pdf-legacy.onrender.com/#");
+
+    cy.get("input[type=file]")
+      .first()
+      .selectFile("cypress/fixtures/black bear.pdf", { force: true });
+
+    cy.wait(300);
+  });
+
+  it("TA-14: Open Knowledge Graph", () => {
+    cy.get(".force-graph-container").should("not.exist");
+    cy.get("#graph-maker").click();
+    cy.get(".force-graph-container").should("be.visible");
+  });
+
+  it("TA-15: Building the Knowledge Graph", () => {
+    Cypress.config("defaultCommandTimeout", 10000);
+
+    cy.get(".force-graph-container").should("not.exist");
+    cy.get("#graph-maker").click();
+
+    cy.intercept(/https\:\/\/api\.semanticscholar\.org\/.*\?/).as("myAlias");
+    cy.wait("@myAlias").then((interception) => {
+      expect(interception.request.url).to.include("semanticscholar");
+    });
+
+    cy.get(".force-graph-container").find("canvas").should("exist");
+  });
+});
