03a9f7db84109547024b1acc77982751cf83a1c4
paolocorsa <paolo.corsa@mail.polimi.it>
2023-01-06 19:13:06 +0100

    Tests added

---
+++ cypress/e2e/crossReference.cy.js
@@ -0,0 +1,49 @@
+describe("Cross-references (TA-17 to TA-19)", () => {
+  beforeEach(() => {
+    cy.visit("https://pdfl-pdf-legacy.onrender.com/#");
+
+    cy.get("input[type=file]")
+      .first()
+      .selectFile("cypress/fixtures/SE2_DD.pdf", { force: true });
+
+    cy.wait(300);
+  });
+
+
+  it("TA-17: Availability Cross-References", () => {
+
+    cy.get("#pdfjs_internal_id_679R").trigger("mouseover");
+    cy.get("#pop-up").should("be.visible");
+
+
+  });
+
+  it("TA-18: Open Cross-References", function() {
+
+    cy.get("#pdfjs_internal_id_679R").trigger("mouseover");
+    cy.get("#pop-up").click();
+    cy.get("#text-layer-reference").should("be.visible");
+    cy.get("#close-btn-reference").should("be.visible");
+
+  });
+
+  it("TA-19: Open Cross-References (images, tables, chapter …)", function() {
+
+
+    cy.get("#pdfjs_internal_id_725R").trigger("mouseover");
+    cy.get("#pop-up-title")
+      .should("have.text","3.2 Administrator interfaces")
+    cy.get("#pop-up-text")
+      .should("have.text", "Figure 29: Administrator Login");
+
+    cy.get("#current-page")
+      .click()
+      .type("{backspace}9{enter}");
+
+    cy.get("#pdfjs_internal_id_222R").trigger("mouseover")
+    cy.get("#pop-up-image")
+      .should("be.visible");
+
+
+  });
+})
