02f54f247c437283633352a3d063cc2bcd210c12
Matteo Visotto <matteo.visotto@mail.polimi.it>
2023-01-06 09:45:15 +0100

    SummaryKeyComponent refactor

--- src/components/SummaryKeyComponents/AbstractAccordionItem.js
+++ src/components/SummaryKeyComponents/AbstractAccordionItem.js
@@ -1,42 +1,19 @@
-import { LOADING_TEXT } from "../../Constants";
+import { AccordionItem } from "./AccordionItem";
 
 /**
  * Class representing the Abstract Accordion Item in DOM
- * @param {HTMLElement} components.abstractText the text container in the accordion
  */
-class AbstractAccordionItem {
-  components = {
-    abstractText: document.querySelector("#abstract-text"),
-  };
-
+class AbstractAccordionItem extends AccordionItem {
   /**
-   * Set the text of the accordion item
-   * @param text text to be set
+   * @constructor
+   * Set superclass parameters
    */
-  setText = (text) => {
-    this.components.abstractText.innerText = text;
-  };
-
-  /**
-   * Set a default error text in case the content cannot be displayed
-   */
-  setError = () => {
-    this.components.abstractText.innerText = "Abstract cannot be loaded";
-  };
-
-  /**
-   * Set loading constant as a text
-   */
-  setLoading = () => {
-    this.components.abstractText.innerText = LOADING_TEXT;
-  };
-
-  /**
-   * Clear the content setting the text to empty string
-   */
-  clear = () => {
-    this.components.abstractText.innerText = "";
-  };
+  constructor() {
+    super(
+      document.querySelector("#abstract-text"),
+      "Abstract cannot be loaded"
+    );
+  }
 }
 
 export { AbstractAccordionItem };
--- src/components/SummaryKeyComponents/AbstractSummaryAccordionItem.js
+++ src/components/SummaryKeyComponents/AbstractSummaryAccordionItem.js
@@ -1,43 +1,27 @@
-import { LOADING_TEXT } from "../../Constants";
 import { textSummarizer } from "../../services/SummarizerService";
+import { AccordionItem } from "./AccordionItem";
 
 /**
  * Class representing the Abstract Summary Accordion Item in DOM
- * @param {HTMLElement} components.abstractSummaryText the text container in the accordion
  */
-class AbstractSummaryAccordionItem {
-  components = {
-    abstractSummaryText: document.querySelector("#abstract-summary-text"),
-  };
+class AbstractSummaryAccordionItem extends AccordionItem {
+  /**
+   * @constructor
+   * Set superclass parameters
+   */
+  constructor() {
+    super(
+      document.querySelector("#abstract-summary-text"),
+      "Abstract summary cannot be generated"
+    );
+  }
 
   /**
-   * Set the text of the accordion item given the full text to be summarized
+   * Set the text af the abstract
    * @param abstract
    */
   setAbstract = (abstract) => {
-    this.components.abstractSummaryText.innerText = textSummarizer(abstract, 6);
-  };
-
-  /**
-   * Set a default error text in case the content cannot be displayed
-   */
-  setError = () => {
-    this.components.abstractSummaryText.innerText =
-      "Abstract summary cannot be generated";
-  };
-
-  /**
-   * Set loading constant as a text
-   */
-  setLoading = () => {
-    this.components.abstractSummaryText.innerText = LOADING_TEXT;
-  };
-
-  /**
-   * Clear the content setting the text to empty string
-   */
-  clear = () => {
-    this.components.abstractSummaryText.innerText = "";
+    this.setText(textSummarizer(abstract, 6));
   };
 }
 
