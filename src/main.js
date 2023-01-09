0e66f86ef27663947502820af57a98b21a13953d
dmesic <dario.mesic@fer.hr>
2023-01-09 17:00:48 +0100

    css code split in multiple files, removed unused lines

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { App } from "./components/AppComponent/App";
 
-import "./styles/fileUpload.css";
+import "./styles/welcomePage.css";
 import "./styles/navbar.css";
 import "./styles/pdfl.css";
 import "./styles/tutorialPage.css";
+import "./styles/textLayer.css";
+import "./styles/sideComponent.css";
 
 App.start();
