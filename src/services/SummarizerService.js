05b5c484679175ddf1491ed8da8c42564961d457
Matteo Visotto <matteo.visotto@mail.polimi.it>
2023-02-21 10:18:24 +0100

    New Summarizer and fix new line on summary

--- src/services/SummarizerService.js
+++ src/services/SummarizerService.js
@@ -1,4 +1,5 @@
 import { SUMMARIZER_STOP_LIST } from "../Constants";
+import { TextRank } from "textrank";
 
 /**
  * @param text {string} text to be summarized
@@ -8,49 +9,55 @@ import { SUMMARIZER_STOP_LIST } from "../Constants";
  * @returns {string} summarized text
  */
 function textSummarizer(text, sentenceNumber) {
-  var documentParts = [];
-  var sentences = text
-    .replace(/\.+/g, ".|")
-    .replace(/\?/g, "?|")
-    .replace(/\!/g, "!|")
-    .split("|");
-  sentences.pop();
-  sentences.forEach(function (sentence) {
-    const wordArray = sentence.split(" ").filter(function (word) {
-      return SUMMARIZER_STOP_LIST.indexOf(word) === -1;
-    });
-    documentParts.push({
-      sentence: sentence,
-      words: wordArray,
-      score: 0,
-    });
-  });
-  documentParts.forEach(function (part) {
-    var count = 0;
-    part.words.forEach(function (word) {
-      const match = word;
-      documentParts.forEach(function (part2) {
-        part2.words.forEach(function (word2) {
-          if (word2 === match) count++;
-        });
+  text = text.replace(/[\r\n]/gm, " ");
+  try {
+    var textRank = new TextRank(text, { extractAmount: 6 });
+    return textRank.summarizedArticle;
+  } catch {
+    var documentParts = [];
+    var sentences = text
+      .replace(/\.+/g, ".|")
+      .replace(/\?/g, "?|")
+      .replace(/\!/g, "!|")
+      .split("|");
+    sentences.pop();
+    sentences.forEach(function (sentence) {
+      const wordArray = sentence.split(" ").filter(function (word) {
+        return SUMMARIZER_STOP_LIST.indexOf(word) === -1;
+      });
+      documentParts.push({
+        sentence: sentence,
+        words: wordArray,
+        score: 0,
       });
     });
-    count = count / part.words.length;
-    part.frequency = count;
-  });
+    documentParts.forEach(function (part) {
+      var count = 0;
+      part.words.forEach(function (word) {
+        const match = word;
+        documentParts.forEach(function (part2) {
+          part2.words.forEach(function (word2) {
+            if (word2 === match) count++;
+          });
+        });
+      });
+      count = count / part.words.length;
+      part.frequency = count;
+    });
 
-  documentParts.sort(function (a, b) {
-    return b.frequency - a.frequency;
-  });
+    documentParts.sort(function (a, b) {
+      return b.frequency - a.frequency;
+    });
 
-  if (documentParts.length >= sentenceNumber) {
-    var result = "";
-    for (var i = 0; i < sentenceNumber; i++) {
-      result = result + documentParts[i].sentence;
+    if (documentParts.length >= sentenceNumber) {
+      var result = "";
+      for (var i = 0; i < sentenceNumber; i++) {
+        result = result + documentParts[i].sentence;
+      }
+      return result;
+    } else {
+      return text;
     }
-    return result;
-  } else {
-    return text;
   }
 }
 
